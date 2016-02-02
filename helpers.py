4e95d48e35bbeb6eeeb7fbe200c734dd1f31d4a2
Timothy Chung <timchunght@gmail.com>
2016-02-02 11:29:00 -0500

    Refactored code to extract adj_close_price into array

--- helpers.py
+++ helpers.py
@@ -1,5 +1,6 @@
 import json
-
+import sys
+from yahoo_finance import Share
 def parse_json(data_filename, ticker_filename):
     try:
         data_file = open(data_filename, "r")
@@ -10,13 +11,53 @@ def parse_json(data_filename, ticker_filename):
         print ("Cannot open file %s\n" % filename)
         sys.exit("bye")
 
+    prices = {}
     tickers = ticker_file.readlines();
-
     for ticker in tickers:
         ticker = ticker.split()[0]
-        print data[ticker]
-        print ticker
+        stock_data = data[ticker]
 
+        prices[ticker] = [0 for j in xrange(len(stock_data))]
+        for j in xrange(len(stock_data)):
+            # print str(j) + " price: " + stock_data[j]['Adj_Close']
+            prices[ticker][j] = stock_data[j]['Adj_Close']
+        print prices[ticker]
     data_file.close()
     ticker_file.close()
 
+def download_json(ticker_filename, output_filename):
+    try:
+        ticker_file = open(ticker_filename, 'r') # opens the input file
+        output_file = open(output_filename, "w")
+    except IOError:
+        print ("Cannot open file %s\n" % ticker_filename)
+        sys.exit("bye")
+
+    lines = ticker_file.readlines();
+
+    count = 1
+    prices = {}
+    for line in lines:
+        thisline = line.split()
+        if len(line) > 0:
+            ticker = thisline[0]
+            print (str(count) + " " + ticker)
+
+            share = Share(ticker)
+            everything = share.get_historical('2015-01-01', '2015-01-31')
+            # prices[thisline[0]] = [0 for j in xrange(len(everything))]
+    #         for j in xrange(len(everything)):
+    # #            print str(j) + " price: " + everything[j]['Adj_Close']
+    #             prices[thisline[0]][j] = everything[j]['Adj_Close']
+            
+            prices[ticker] = everything
+            print ticker
+            count += 1
+            
+        if count == 5:
+            break
+
+    json.dump(prices, output_file)          
+
+    ticker_file.close()
+    output_file.close()
--- myprices.py
+++ myprices.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 import sys
 import json
-from yahoo_finance import Share
 from pprint import pprint
+import helpers
 
 
 if len(sys.argv) != 2:  # the program name and the datafile
@@ -13,38 +13,4 @@ filename = sys.argv[1]
 
 print "input", sys.argv[1]
 
-try:
-    f = open(filename, 'r') # opens the input file
-    output = open("output", "w")
-except IOError:
-    print ("Cannot open file %s\n" % filename)
-    sys.exit("bye")
-
-lines = f.readlines();
-
-count = 1
-prices = {}
-for line in lines:
-    thisline = line.split()
-    if len(line) > 0:
-        ticker = thisline[0]
-        print (str(count) + " " + ticker)
-
-        share = Share(ticker)
-        everything = share.get_historical('2015-01-01', '2015-01-31')
-        # prices[thisline[0]] = [0 for j in xrange(len(everything))]
-#         for j in xrange(len(everything)):
-# #            print str(j) + " price: " + everything[j]['Adj_Close']
-#             prices[thisline[0]][j] = everything[j]['Adj_Close']
-        
-        prices[ticker] = everything
-        print ticker
-        count += 1
-        
-    if count == 5:
-        break
-
-json.dump(prices, output)          
-
-f.close()
-output.close()
+helpers.download_json(filename, "data.json")
