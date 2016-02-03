62ea7752b6ee8f511bd85268c0d7ffdc6f909114
Timothy Chung <timchunght@gmail.com>
2016-02-03 18:35:04 -0500

    Changed downloader to accept two arguments

--- download_stock_prices.py
+++ download_stock_prices.py
@@ -5,12 +5,13 @@ from pprint import pprint
 import helpers
 
 
-if len(sys.argv) != 2:  # the program name and the datafile
+if len(sys.argv) != 3:  # the program name and the datafile
   # stop the program and print an error message
-  sys.exit("usage: read1.py datafile ")
+  sys.exit("usage: read1.py data_file output_name.json")
 
 filename = sys.argv[1]
+output_filename = sys.argv[2]
+print "Input: ", sys.argv[1]
+print "Output: ", sys.argv[2]
 
-print "input", sys.argv[1]
-
-helpers.download_json(filename, "data.json")
+helpers.download_json(filename, output_filename)
