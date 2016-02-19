caeacde07104a43cae9996316e445be94d859711
Timothy Chung <timchunght@gmail.com>
2016-02-19 16:53:47 -0500

    Added myopt folder

---
+++ myopt/src/myopt.h
@@ -0,0 +1,31 @@
+#ifndef MYO
+
+#define MYO
+
+typedef struct myo{
+  int n;
+  int f;
+  int iteration;
+  double *mu;
+  double *sigma2;
+  double *V;
+  double *upper;
+  double *lower;
+  double *F;
+  double *gradient;
+  double *x;
+  double *Vx;
+  double *VtF;
+}myo;
+
+#define NOMEM 100
+#define DATAERROR1 101
+ 
+int myoGetmyoFromFile(myo **ppmyo, char *filename);
+int myocreatemyo(myo **pmyo);
+void myokillmyo(myo **ppmyo);
+int myoalgo(myo *pmyo);
+
+#include "utilities.h"
+
+#endif
